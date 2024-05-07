35da731ebfd5e135cd3ed36b9dd88e84a90a2e92
MAURICIO <MAURICIO@DESKTOP-EKS03OE>
2024-05-06 23:13:20 -0300

    Ejercicio 5 resuelto

---
+++ Tp2_GutierrezMauricio/src/ar/edu/unju/fi/ejercicio5/model/Producto.java
@@ -0,0 +1,93 @@
+package ar.edu.unju.fi.ejercicio5.model;
+
+
+public class Producto {
+	private String codigo;
+	private String descripcion;
+	private double precioUnitario;
+	private OrigenFabricacion origenFabricacion;
+	private Categoria categoria;
+	private boolean disponible;
+	
+	public Producto(String codigo, String descripcion, double precioUnitario,
+			OrigenFabricacion origenFabricacion,Categoria categoria, boolean disponible) {
+		this.codigo = codigo;
+		this.descripcion = descripcion;
+		this.precioUnitario = precioUnitario;
+		this.origenFabricacion = origenFabricacion;
+		this.categoria = categoria;
+		this.disponible = disponible;
+		
+	}
+	
+	//DECLARACION DE LOS ENUMS****
+
+	public enum OrigenFabricacion{
+		ARGENTINA, CHINA, BRASIL, URUGUAY;
+	}
+	
+	public enum Categoria{
+		TELEFONIA, INFORMATICA, ELECTROHOGAR, HERRAMIENTAS;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public double getPrecioUnitario() {
+		return precioUnitario;
+	}
+
+	public void setPrecioUnitario(double precioUnitario) {
+		this.precioUnitario = precioUnitario;
+	}
+
+	public OrigenFabricacion getOrigenFabricacion() {
+		return origenFabricacion;
+	}
+
+	public void setOrigenFabricacion(OrigenFabricacion origenFabricacion) {
+		this.origenFabricacion = origenFabricacion;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public boolean isDisponible() {
+		return disponible;
+	}
+
+	public void setDisponible(boolean disponible) {
+		this.disponible = disponible;
+	}
+	
+	//GETTERS AND SETTERS 
+	
+    public String toString() {
+        return "Producto- " +
+                "Codigo='" + codigo + '\'' +
+                ", Descripcion='" + descripcion + '\'' +
+                ", Precio Unitario=" + precioUnitario +
+                ", Origen Fabricacion=" + origenFabricacion +
+                ", Categoria=" + categoria +
+                ", Disponible=" + disponible +
+                '.';
+    }
+}
